c674f2de7d0a0687b99e0e6a3e21ed9d1b6d914f
Kayque Silva Della Capa <ksdellacapa@gmail.com>
2023-11-03 15:05:42 -0300

    Add files via upload

---
+++ BibliotecaMatematica/Matematica.cpp
@@ -0,0 +1,35 @@
+#include "Matematica.h"
+#include <cmath>
+
+int Soma(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+int Subtracao(int num1, int num2)
+{
+	return num1 - num2;
+}
+
+int Multiplicacao(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+float Divisao(int num1, int num2)
+{	
+	//Faz uma divisão inteira pois num1 e num2 são inteiros
+	// mas podemos fazer cast
+	// agora estamos indicando que o inteiro deve ser um float. É um cast
+	return (float)num1 / (float)num2;
+}
+
+double Potencia(double num1, double num2)
+{
+	return pow(num1, num2); // Função pow faz elevação
+}
+
+float RaizQuadrada(int num)
+{
+	return sqrt(num);
+}
